b42f8df2abbda4310fe3a7fe62ea04f42cb9ef53
Daniel Benzion <daniel.z.benzion@gmail.com>
2021-10-29 09:31:41 -0500

    Shorten pipe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { FilterPipe } from './filter.pipe';
 import { SortPipe } from './sort.pipe';
+import { ShortenPipe } from './shorten.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
     FilterPipe,
-    SortPipe
+    SortPipe,
+    ShortenPipe
   ],
   imports: [
     BrowserModule,
